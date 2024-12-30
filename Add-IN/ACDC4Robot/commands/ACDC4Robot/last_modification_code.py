e066b79eb4792ab18f52f13b23706a7e4a43aaab
johni <39644164+JOHNI1@users.noreply.github.com>
2024-12-30 16:59:51 +0200

    recovered accidentally removed code!

--- Add-IN/ACDC4Robot/commands/ACDC4Robot/last_modification_code.py
+++ Add-IN/ACDC4Robot/commands/ACDC4Robot/last_modification_code.py
@@ -1,3 +1,53 @@
+# Add the comment and plugin to the front
+comment = Comment("frame_class_type : hexa-x")
+robot_ele.insert(0, comment)
+
+gazebo_ele = Element("gazebo")
+gz_sim_joint_state_publisher_system = SubElement(gazebo_ele, "plugin")
+gz_sim_joint_state_publisher_system.attrib = {"filename": "gz-sim-joint-state-publisher-system", "name": "gz::sim::systems::JointStatePublisher"}
+
+robot_ele.insert(1, gazebo_ele)
+
+# Iterate through all links and print their names
+for link in robot_ele.iter("link"):
+    futil.log(link.attrib['name'])
+
+gz_sim_apply_joint_force_system_rotors_list = []
+
+# Iterate through all joints
+for joint in robot_ele.iter("joint"):
+    futil.log(joint.attrib['name'])
+    if "spring" in joint.attrib['name']:
+
+        # Add damping and friction for spring joints
+        dynamics = SubElement(joint, "dynamics")
+        dynamics.attrib = {"damping": "10", "friction": "0.0"}
+
+        # Add gazebo reference for the spring joints
+        gazebo_ele = Element("gazebo")
+        gazebo_ele.attrib = {"reference": joint.attrib['name']}
+        spring_stiffness = SubElement(gazebo_ele, "springStiffness")
+        spring_stiffness.text = "500"
+        spring_reference = SubElement(gazebo_ele, "springReference")
+        spring_reference.text = "0"
+        robot_ele.append(gazebo_ele)
+
+    # Collect rotor joints for applying forces later
+    if "rotor" in joint.attrib['name'] and "prop" not in joint.attrib['name']:
+        gz_sim_apply_joint_force_system_rotors_list.append(joint.attrib['name'])
+
+# Add the plugin for applying forces to the rotors
+gazebo_ele = Element("gazebo")
+for joint in gz_sim_apply_joint_force_system_rotors_list:
+    gz_sim_apply_joint_force_system = SubElement(gazebo_ele, "plugin")
+    gz_sim_apply_joint_force_system.attrib = {"filename": "gz-sim-apply-joint-force-system", "name": "gz::sim::systems::ApplyJointForce"}
+    joint_name = SubElement(gz_sim_apply_joint_force_system, "joint_name")
+    joint_name.text = joint
+
+robot_ele.append(gazebo_ele)
+
+# Define IMU and related elements under the existing structure using Element and SubElement
+
 # Create IMU joint element
 imu_joint = Element("joint")
 imu_joint.attrib = {"name": "imu_joint", "type": "revolute"}
